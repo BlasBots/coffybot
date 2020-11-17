acedd050b034d7821f623838432e1faaa67b0ead
aronxd <48607514+aronxd@users.noreply.github.com>
2020-11-16 23:29:59 -0300

    Delete index.shard.js

--- index.shard.js
+++
@@ -1,4 +0,0 @@
-let { ShardingManager } = require('discord.js'),
-  manager = new ShardingManager('./index.js', { token: require('./config.js').token, totalShards: 'auto' });
-manager.spawn();
-manager.on('shardCreate', (shard) => console.log(`Shard #${shard.id} ejecutado`));
